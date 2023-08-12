9c73872279e0a17193dfda1ed8df8e6af16065b1
Anik Das <61902916+Anik15@users.noreply.github.com>
2023-08-12 22:45:05 +0530

    Update Day1_String_Assignment.py

--- Namaste Python Course/Day1_String_Assignment.py
+++ Namaste Python Course/Day1_String_Assignment.py
@@ -1,18 +1,18 @@
-# 1- write a program which takes 2 inputs from the user : weight(kg) and height(meter) and prints the BMI in the output.
+# 1- write a program that takes 2 inputs from the user: weight(kg) and height(meter) and prints the BMI in the output.
 # BMI = weight / (square of height)
 import math as m
 weight = float(input("Please Enter your Weight(kg): "))
 height = float(input("Please Enter your Height(meter): "))
-bmi = round(weight/m.pow(height,2),2)
-print("Your BMI is: ",bmi)
+BMI = round(weight/m.pow(height,2),2)
+print("Your BMI is: ",BMI)
 
-# 2- write a program which takes the name of the user as input and
-# replace all the occurence of character 'a' in the name to 'b' and print it.
+# 2- write a program that takes the name of the user as input and
+# replace all the occurrences of character 'a' in the name to 'b' and print it.
 name=str(input("Please Enter your name: "))
 name = name.lower().replace('a','b')
 print("Replaced Name: ",name)
 
-# 3- write a program which takes 2 inputs from user as principle amount (int) and rate of annual interest (float) and
+# 3- write a program that takes 2 inputs from the user as the principal amount (int) and rate of annual interest (float) and
 # print the expected total amount  after  2 years.
 p = int(input("Please Enter the principal Amount: "))
 r = float(input("Please Enter the Interest: "))
@@ -21,14 +21,14 @@ print("Return in simple interest: ",si)
 ci = p*pow((1 + r/100),2)
 print("Return in compound interest: ",ci)
 
-# 4- write a program which takes city name from user input. irrespective of in which case user enters the city name,
-# print the city name in camel case meaning first letter should be capital and rest in small.
-# example : input : MYSORE ,  print - > Mysore
+# 4- write a program that takes the city name from user input. irrespective of in which case the user enters the city name,
+# print the city name in camel case meaning the first letter should be capitalized and rest in small.
+# example: input: MYSORE,  print - > Mysore
 city = str(input("Please Enter the city: "))
 city = city.capitalize()
 print("City name in Camel Case: ",city)
 
-# 5- write a program which takes the name of the user as input and
+# 5- write a program that takes the name of the user as input and
 # print the index of character 'a' in the string. if 'a' is not there then return -1.
 name = input("Enter your name: ")
 index = name.lower().find('a')
@@ -38,13 +38,13 @@ print("Index of 'a':" + str(index))
 my_word = "antidisestablishmentarianism"
 print("Length of my word: ",len(my_word))
 
-# 7- take 3 inputs from user : first name , last name and age . Display the information in below format
+# 7- take 3 inputs from the user: first name, last name, and age. Display the information in the below format
 # exmaple
-# first name : MOhit
-# last name : sharma
+# first name: MOhit
+# last name: sharma
 # age 32
-# Display : my name is Mohit Sharma and I am 32 years old.
-# note that first letter of first name and last name both should be in capital letters and rest in small.
+# Display: my name is Mohit Sharma and I am 32 years old.
+# Note that the first letter of the first name and last name both should be in capital letters and the rest in small.
 
 first_name = str(input("Please Enter your First Name: "))
 last_name = str(input("Please Enter your Last Name: "))
@@ -53,14 +53,14 @@ lc_name = last_name.capitalize()
 age =int(input("Please Enter your age: "))
 print(f"My name is {fc_name} {lc_name} and I am {age} years old.")
 
-# 8-take 3 inputs from user : first name , last name and company name.
+# 8-take 3 inputs from the user: first name, last name, and company name.
 # create the email alias for the user and display it.
-# Email alias is first 2 letters of first name , last 3 letters of last name and @company.com
+# Email alias is first 2 letters of first name, last 3 letters of last name and @company.com
 # example
-# first name : MOhit
-# last name : sharma
-# company : infosys
-# Display : morma@infosys.com
+# first name: MOhit
+# last name: sharma
+# company: infosys
+# Display: morma@infosys.com
 # note full email id should -be in lower case
 
 first_name = str(input("Please Enter your First Name: "))
